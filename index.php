69c4e93b296fbfd90444efcb47b33b5568086f8b
rina <info@wdfrelancer.com>
2024-12-19 19:26:06 +0530

    removed content

--- index.php
+++ index.php
@@ -118,26 +118,7 @@
             <path d="M12 2a10 10 0 1 0 10 10A10 10 0 0 0 12 2Zm1 15h-2v-2h2Zm0-4h-2V7h2Z" />
         </svg>
         <p>Track your time, manage your projects, and improve productivity with our easy-to-use tools. Accessible via both desktop and web.</p>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis veniam illum, amet dignissimos deserunt aperiam ipsam asperiores vel sed voluptas dolorum facere minima soluta exercitationem quae aut optio, temporibus error.
-        A distinctio ea ipsa deserunt aperiam officia similique iste inventore perspiciatis. Adipisci, voluptas nisi. Tempora fugit, cumque ullam impedit ipsum deserunt ipsa natus expedita, optio qui provident, maiores harum dolorem.
-        Pariatur id, totam, et, ipsum laboriosam repudiandae ipsa culpa quod mollitia quo iste quisquam architecto quis blanditiis sunt labore? Harum qui illo dolorum recusandae obcaecati, eligendi veniam nam non error.
-        Quo a vero minima harum natus deleniti repellat beatae adipisci voluptates quasi blanditiis quas fugit dolorem autem earum unde, culpa iste eum sequi corporis? Reiciendis earum laudantium ex nemo iure?
-        Eligendi nemo cum delectus officia vitae fugiat recusandae vel nesciunt optio a maiores numquam incidunt labore, magni earum culpa quasi voluptatum tenetur exercitationem reiciendis iusto aliquam nulla sunt? Facilis, officiis?
-        Dicta dolorem incidunt in? Debitis vero aliquam voluptate inventore, dolorum sit! Necessitatibus expedita laborum deleniti dolores voluptatibus ullam tempore veritatis reiciendis est! Voluptates tempore atque molestias magnam, assumenda excepturi. Rerum!
-        Blanditiis est dignissimos, amet accusamus asperiores cum beatae nulla! Quaerat quos voluptatum saepe itaque tempore accusantium perspiciatis tempora, nulla earum, vero sit suscipit quas officia dicta est voluptas. Amet, sequi!
-        Iusto, dolorum hic excepturi ea adipisci rerum expedita magni velit illum facilis doloremque consequatur fuga quod quisquam alias quas corrupti iste atque? Suscipit exercitationem, repellat quo culpa inventore laudantium voluptas.
-        Nam non ut porro illum rerum corrupti accusantium rem sapiente laboriosam impedit quae doloribus similique tenetur delectus veniam, consectetur cumque, id modi animi exercitationem. Consectetur cumque ducimus repellendus quidem impedit!
-        Blanditiis, alias, aut quia numquam nulla exercitationem obcaecati omnis, eos voluptas ullam enim nobis beatae ad explicabo hic velit ipsa molestias ducimus perspiciatis. Eligendi id porro quisquam voluptate ducimus impedit!
-        Laudantium soluta saepe aspernatur eaque sapiente suscipit, doloremque et porro recusandae est tempora numquam commodi at corporis error molestias odit! Vel facere eum veritatis aliquam reprehenderit eligendi eaque unde laudantium.
-        Delectus cupiditate iure est asperiores vel maxime, exercitationem numquam, error aperiam ab consequuntur, explicabo repellat quas! Quasi eos illum accusantium, porro molestias nesciunt non, laborum, voluptatum fugit voluptatem sequi est?
-        Incidunt ab esse voluptatibus eius, ex deleniti? Blanditiis neque totam quidem molestiae adipisci delectus doloribus quasi, rerum sapiente exercitationem sit consequuntur commodi? Accusamus rem harum commodi atque. Voluptatibus, porro quae.
-        Harum autem placeat quis. Rem, voluptatum repudiandae doloribus corrupti praesentium quaerat aut. Nobis nostrum eligendi officiis culpa minus, atque hic amet nesciunt eveniet facere vel veniam dolorum, cum optio aperiam.
-        Alias recusandae error temporibus rerum quam cupiditate nemo dolorem tempora atque, eos culpa! Accusamus culpa eius quae eligendi minus odio facere vel ut quisquam, molestiae nostrum rerum, consequatur itaque numquam!
-        Assumenda, repellat qui! Beatae sint quidem ea eos ullam nihil quisquam praesentium. Commodi soluta blanditiis laudantium, similique, voluptatum odio laborum, dolorum nisi esse voluptas accusamus ipsam dolorem consectetur deleniti illo.
-        Suscipit quo ut tempore laboriosam ducimus commodi. Nobis at iure sint alias exercitationem harum repellat ipsum minima nemo. Magnam reiciendis maxime eos ea voluptate mollitia odio earum cum doloribus aliquam!
-        Sint deserunt eius qui veritatis exercitationem iste rerum molestias, eveniet explicabo tempora quasi molestiae facilis optio aliquid culpa dignissimos voluptatibus sunt nobis in sequi quaerat? Fugit adipisci officiis quam odio?
-        Aliquid rem voluptates voluptatem, et quod ad enim animi impedit natus recusandae sapiente excepturi repellat molestiae harum! Explicabo ducimus maiores modi autem, velit ab sint vitae placeat ipsam sequi reprehenderit?
-        Perspiciatis non eaque, odit incidunt totam deleniti blanditiis et magni, accusantium quidem, inventore alias corrupti nulla illo maiores nihil ipsum voluptate molestias consequatur quia voluptatem. Assumenda mollitia possimus est ea.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis veniam illum, amet dignissimos deserunt aperiam ipsam</p>
     </div>
 
     <script>
