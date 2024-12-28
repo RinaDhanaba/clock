d66ffd1c8f980c11cc11e41784f69d2760bc38eb
rina <info@wdfrelancer.com>
2024-12-28 15:59:14 +0530

    debug

--- db.php
+++ db.php
@@ -1,4 +1,9 @@
 <?php
+
+SELECT User, Host FROM mysql.user WHERE User = 'u649003729_S7DVl';
+GRANT USAGE ON *.* TO 'u649003729_S7DVl'@'193.203.184.109' IDENTIFIED BY 'FjfTJ@Bnwi7N25YC';
+GRANT ALL PRIVILEGES ON u649003729_BUQNI.* TO 'u649003729_S7DVl'@'193.203.184.109';
+
 $host = '193.203.184.109';
 $db = 'u649003729_BUQNI';
 $user = 'u649003729_S7DVl';
@@ -10,4 +15,7 @@ try {
 } catch (PDOException $e) {
     die("Could not connect to the database: " . $e->getMessage());
 }
+
+FLUSH PRIVILEGES;
+
 ?>
