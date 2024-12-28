421e9d480a2afbf7003cad20756764729c85f792
rina <info@wdfrelancer.com>
2024-12-28 10:49:25 +0530

    added style

---
+++ main/copyright.php
@@ -0,0 +1,2 @@
+
+    <p>&copy; <?php echo date("Y"); ?> My Website. All Rights Reserved.</p>
--- main/head.php
+++ main/head.php
@@ -1,3 +1,5 @@
 <link rel="stylesheet" href="styles.css">
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link rel="stylesheet" href="style/global.css">
+<script src="script/global.js" defer></script>
