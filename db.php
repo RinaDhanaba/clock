c0c71c17b2207e25750ebb6534eb86e49a3dacdb
rina <info@wdfrelancer.com>
2024-12-28 15:43:44 +0530

    db password added

--- db.php
+++ db.php
@@ -2,7 +2,7 @@
 $host = 'srv1552.hstgr.io';
 $db = 'u649003729_BUQNI';
 $user = 'u649003729_S7DVl';
-$pass = '';
+$pass = 'FjfTJ@Bnwi7N25YC';
 
 try {
     $pdo = new PDO("mysql:host=$host;dbname=$db", $user, $pass);
