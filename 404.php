96f320032e23e2e6ea67bc5b98f8078c76610643
rina <info@wdfrelancer.com>
2024-12-28 14:24:44 +0530

    added htacces file and 404 page

---
+++ 404.php
@@ -0,0 +1,11 @@
+<head>
+    <?php include 'main/head.php';?>
+    <?php include 'main/meta.php';?>
+</head>
+<body class="light">
+<?php include 'main/header.php';?>
+
+<h1>404 Page</h1>
+
+<?php include 'main/footer.php';?>
+<?php include 'main/copyright.php';?>
