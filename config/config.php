5c61d7d3c5225f577b0291b3578676e1449944ff
RinaDhanaba <98539420+RinaDhanaba@users.noreply.github.com>
2025-02-02 15:39:03 +0530

    removed client id

--- config/config.php
+++ config/config.php
@@ -11,8 +11,8 @@ define('DB_PASS', 'FjfTJ@Bnwi7N25YC');
 define('DB_NAME', 'u649003729_BUQNI');
 
 // Google API settings (replace with your actual Google client ID and secret)
-define('GOOGLE_CLIENT_ID', '963000999551-mgcts123rbbjj44e8ebakdctdf3attsc.apps.googleusercontent.com');
-define('GOOGLE_CLIENT_SECRET', ' ');
+define('GOOGLE_CLIENT_ID', '');
+define('GOOGLE_CLIENT_SECRET', '');
 define('GOOGLE_REDIRECT_URI', BASE_URL . 'google-callback.php');
 
 // Other settings
