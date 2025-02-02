b0c8fb9b41071aa63ba3a01247839cd7874adcd6
RinaDhanaba <98539420+RinaDhanaba@users.noreply.github.com>
2025-02-02 15:56:40 +0530

    new config

---
+++ config/config.php
@@ -0,0 +1,14 @@
+<?php
+// config/config.php
+
+// Application base URL (adjust to your domain/path)
+define('BASE_URL', 'https://clock.wdfreelancer.com/');
+
+// Database settings
+define('DB_HOST', '127.0.0.1:3306');
+define('DB_USER', 'u649003729_S7DVl');
+define('DB_PASS', 'FjfTJ@Bnwi7N25YC');
+define('DB_NAME', 'u649003729_BUQNI');
+
+// Other settings
+session_start();
